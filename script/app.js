8642b30057e1073dbc8c2d6816d3f790b45d5c4d
Afeez Bolaji <afeezbolajiola@gmail.com>
2025-01-23 18:06:28 +0100

    Having fun

--- script/app.js
+++ script/app.js
@@ -4,22 +4,22 @@ const fullName = document.getElementById("fullName");
 const userName = document.getElementById("username");
 const imageInput = document.getElementById("imageInput");
 const imagePreview = document.getElementById("imagePreview")
+const uploadButtons = document.getElementById("uploadButtons");
 
 const uploadErrorElement = document.getElementById("uploadError");
 const emailErrorElement = document.getElementById("emailError");
 
 imageInput.addEventListener('change', function(event) {
-      const file = event.target.files[0]; // Get the selected file
+      const file = event.target.files[0]; 
       if (file) {
         const reader = new FileReader();
-
-        // Once the file is read, set it as the source of the image preview
         reader.onload = function(e) {
-          imagePreview.src = e.target.result; // Set the image preview source
-          console.log(imagePreview.src);
+          imagePreview.src = e.target.result; 
+          console.log(imagePreview);
+          uploadButtons.style.display = "flex";
         };
 
-        reader.readAsDataURL(file); // Read the file as a data URL
+        reader.readAsDataURL(file); 
       }
     });
 
