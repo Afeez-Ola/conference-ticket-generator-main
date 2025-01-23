e3c2caec9959a0f5a5371837c125246be4b1357b
Afeez Bolaji <afeezbolajiola@gmail.com>
2025-01-23 15:10:26 +0100

    Having fun

---
+++ script/app.js
@@ -0,0 +1,7 @@
+const form  = document.getElementById("form")
+const email = document.getElementById("email");
+const fullName = document.getElementById("fullName");
+const userName = document.getElementById("username");
+
+const uploadErrorElement = document.getElementById("uploadError");
+const emailErrorElement = document.getElementById("emailError");
